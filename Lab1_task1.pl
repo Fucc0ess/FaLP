eb061f536acaaa7ce4b9679825c7a4c8b1cd1446
Малакеев Владимир Дмитриевич <108796425+Fucc0ess@users.noreply.github.com>
2024-02-06 11:17:36 +0300

    Задание 1 Лабораторной работы 1
    Составить базу данных Prolog (набор фактов) о поле всех членов семьи (к примеру man, woman). Написать несколько запросов к терминалу на проверку пола конкретных членов семьи.
    Построить предикаты men и women, которые выводят на экран всех мужчин и всех женщин соответственно. Провести трассировку.
    Построить базу данных Prolog отношения «является родителем. Провести несколько запросов к терминалу Prolog. Построить предикат, который children(X), который выводит всех детей X.
    Построить предикат mother(X, Y), который проверяет, является ли X матерью Y.
    Построить предикат, mother(X), который выводит маму X.
    Построить предикат brother(X, Y), который проверяет, является ли X братом Y.
    Построить предикат brothers(X), который выводит всех братьев X.
    Построить предикат b_s(X,Y), который проверяет, являются ли X и Y родными братом и сестрой или братьями или сестрами. Построить предикат b_s(X), который выводит всех братьев или сестер X.

---
+++ Lab1_task1.pl
@@ -0,0 +1,71 @@
+man(voeneg).
+man(ratibor).
+man(boguslav).
+man(velerad).
+man(duhovlad).
+man(svyatoslav).
+man(dobrozhir).
+man(bogomil).
+man(zlatomir).
+man(miloslav).
+
+woman(goluba).
+woman(lubomila).
+woman(bratislava).
+woman(veslava).
+woman(zhdana).
+woman(bozhedara).
+woman(broneslava).
+woman(veselina).
+woman(zdislava).
+woman(bogdana).
+
+parent(voeneg,ratibor).
+parent(voeneg,bratislava).
+parent(voeneg,velerad).
+parent(voeneg,zhdana).
+
+parent(goluba,ratibor).
+parent(goluba,bratislava).
+parent(goluba,velerad).
+parent(goluba,zhdana).
+
+parent(miloslav, lubomila).
+parent(miloslav, boguslav).
+parent(miloslav, veslava).
+parent(miloslav, duhovlad).
+
+parent(bogdana, lubomila).
+parent(bogdana, boguslav).
+parent(bogdana, veslava).
+parent(bogdana, duhovlad).
+
+parent(ratibor,svyatoslav).
+parent(ratibor,dobrozhir).
+parent(lubomila,svyatoslav).
+parent(lubomila,dobrozhir).
+
+parent(boguslav,bogomil).
+parent(boguslav,bozhedara).
+parent(bratislava,bogomil).
+parent(bratislava,bozhedara).
+
+parent(velerad,broneslava).
+parent(velerad,veselina).
+parent(veslava,broneslava).
+parent(veslava,veselina).
+
+parent(duhovlad,zdislava).
+parent(duhovlad,zlatomir).
+parent(zhdana,zdislava).
+parent(zhdana,zlatomir).
+
+men :- man(X), write(X), nl, fail. %men - вывод всех мужчин
+women :- woman(X), write(X), nl, fail. %women - вывод всех женщин
+children(X) :- parent(X, Y), write(Y), nl, fail. %children(+X) - вывод всех детей X
+mother(X, Y) :- parent(X, Y), woman(X). %mother(+X, +Y) - проверка является ли X матерью Y
+mother(X) :- parent(Y, X), woman(Y), write(Y). %mother(+X) - вывод матери Y
+brother(X, Y) :- parent(Z, X), parent(Z, Y), man(X). %brother(+X, +Y) - проверка является ли X братом Y
+brothers(X) :- parent(Z, X), parent(Z, Y), not(X==Y), man(Z), man(Y), write(Y), nl, fail. %brothers(+X) - вывод всех братьев X
+b_s(X, Y) :- parent(Z, X), parent(Z, Y). %b_s(+X, +Y) - проверка являются ли X и Y братьями, или сестрами, или братом и сестрой
+b_s(X) :- parent(Z, X), parent(Z, Y), not(X==Y), man(Z), write(Y), nl, fail. %b_s(+X) - вывод всех братьев и сестёр X
